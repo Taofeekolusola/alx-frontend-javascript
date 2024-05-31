bc233401ecce1f5c9931cce12743839583c40ec8
Taofeek Olusola <olusolataofeek@gmail.com>
2024-05-31 01:33:00 +0100

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,22 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
