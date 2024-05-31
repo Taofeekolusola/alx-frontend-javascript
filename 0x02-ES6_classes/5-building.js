e73858525237d5750a5e16acbe25251720127c14
Taofeek Olusola <olusolataofeek@gmail.com>
2024-05-31 01:12:43 +0100

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,36 @@
+class Building {
+  constructor (sqft) {
+    if (this.constructor === Building) {
+      throw new Error("Building is an abstract class and cannot be instantiated directly");
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft () {
+    return this._sqft;
+  }
+
+  // Abstract method
+  evacuationWarningMessage () {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+class OfficeBuilding extends Building {
+  constructor (sqft, name) {
+    super (sqft);
+    this._name = name;
+  }
+
+  get name () {
+    return this._name;
+  }
+
+  evacuationWarningMessage () {
+    return `Evacuate the ${this._name} building immediately!`;
+  }
+}
