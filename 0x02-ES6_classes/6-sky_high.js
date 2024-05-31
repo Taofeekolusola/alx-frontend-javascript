4b5870347f7010904354581c0c9801bda7cba96a
Taofeek Olusola <olusolataofeek@gmail.com>
2024-05-31 01:28:39 +0100

    0x02-ES6_classes

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,40 @@
+class Building {
+  constructor(sqft) {
+    if (this.constructor === Building) {
+      throw new Error("Building is an abstract class and cannot be instantiated directly");
+    }
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square footage must be a number');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  // Abstract method
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
+
+class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    if (typeof floors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    }
+
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
