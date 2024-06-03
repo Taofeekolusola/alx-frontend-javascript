0d51f52d10be5b85e2982629bf56f0312082bbc3
Taofeek Olusola <olusolataofeek@gmail.com>
2024-06-03 18:51:44 +0100

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -10,3 +10,4 @@ function getResponseFromAPI() {
     }
     return apiCall;
 }
+export default getResponseFromAPI;
--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -10,3 +10,4 @@ function getFullResponseFromAPI(success) {
         }
     });
 }
+export default getFullResponseFromAPI;
