88069336594ccd8c1f39cf67f7a36a53a82b5a68
Taofeek Olusola <olusolataofeek@gmail.com>
2024-06-03 17:31:22 +0100

    0x01-ES6_promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,15 +1,12 @@
 function getResponseFromAPI() {
     const apiCall = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const success = true;
+	const success = true;
 
             if (success) {
                 resolve('Data received successfully');
             } else {
                 reject('Error: Unable to fetch data');
             }
-        }, 2000);
-    });
-
+    }
     return apiCall;
 }
